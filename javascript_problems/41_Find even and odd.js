c8d9f48a8330356835184d2f011cb1420ccef8be
shaik.inthiyaz <shaikinthiyaz7729@gmail.com>
2026-01-11 23:52:11 +0530

    add functions to determine even/odd and calculate area of a circle

---
+++ javascript_problems/41_Find even and odd.js
@@ -0,0 +1,10 @@
+function evod  (num){
+   if(num % 2 === 0){
+       return "Even"
+   }
+   else{
+         return "Odd"
+   }
+}
+console.log(evod(5));
+console.log(evod(10));
---
+++ javascript_problems/42_Find the radius area.js
@@ -0,0 +1,4 @@
+function area(radius) {
+    return Math.PI * radius * radius
+}
+console.log(area(12));
